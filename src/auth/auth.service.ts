36205c47d7f033be6453bcdabccb31c29b8d7c55
Mehmet Özdemir <mehmet.ozdemir.boun@gmail.com>
2026-05-20 00:16:31 +0300

    Style errors corrected.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
